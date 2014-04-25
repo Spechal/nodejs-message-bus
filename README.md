2815046788f54526022bce4078292a5fa18641ec
spechal <travis.crowder@spechal.com>
2014-04-24 23:35:46 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
 nodejs-message-bus
 ==================
 
-A simple email message bus http server
+A simple email message bus http server.  Send a request with valid values and send some emails.
+
+#Usage
+
+Basic usage:
+Get the code and install the required dependencies, which is nodemailer
+```
+git clone https://github.com/Spechal/nodejs-message-bus.git
+cd nodejs-message-bus
+npm install
+```
+Configure the bin/server.js allowed_emails list variable to include groups of email addresses to send to.  **The name of the group is the to value in your POST request.**
+
+Start the server; it runs on 127.0.0.1:4321 by default.
+```
+node bin/server.js
+```
+Post to the server with the following parameters:
+```json
+{
+ to: 'group-key-value',
+ subj: 'Subject',
+ body: 'Message'
+}
+```
+i.e.
+```
+curl --data "to=group1&body=some message\n\n-root&subj=new subject" http://127.0.0.1:4321
+```
---
+++ bin/server.js
@@ -0,0 +1,172 @@
+/**
+ * message-bus: A simple email message bus http server
+ * @author Travis Crowder <travis.crowder@spechal.com>
+ */
+
+/**
+ * would be nice to have:
+ * - fire routine ... emails everyone on all email lists
+ * - fix the res.end() calls ... just need one at the end I believe
+ */
+
+/**
+ * Hashmap of array of addresses the program can send to
+ * i.e. If a user submits to=group1, the addresses in the "group1" array will be mailed
+ */
+var allowed_emails = {
+    'group1': [
+        'user@domain.com'
+    ]
+}
+
+/**
+ * Get data via POST or GET (GET seems to be broken ... querystring package issue?)
+ * @type {string}
+ */
+var requestType = 'POST';
+
+/**
+ * Port to listen on
+ * @type {number}
+ */
+var httpPort = 4321;
+
+/**
+ * nodemailer SMTP options
+ */
+var smtpOptions = {
+
+}
+
+/**
+ * nodemailer mail options
+ */
+var mailerOptions = {
+    from: 'root <root@localhost>',
+    subject: 'Message from message-bus',
+    text: 'Default message'
+}
+
+/**
+ * PHP in_array
+ * @param needle
+ * @param haystack
+ * @returns {boolean}
+ */
+function in_array(needle, haystack){
+    if(haystack instanceof Array){
+        for(var i = 0; i < haystack.length; i++){
+            if(haystack[i] == needle){
+                return true;
+            }
+        }
+    } else {
+        for(var p in haystack){
+            if(haystack[p] === needle){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+/**
+ * PHP array_keys
+ * @param data
+ * @returns {*}
+ */
+function array_keys(data){
+    if(data instanceof Array){
+        var ret = [];
+        for(var i = 0; i < data.length; i++){
+            ret.push(data[i]);
+        }
+        return ret;
+    } else if(data instanceof Object){
+        return Object.keys(data);
+    }
+    return false;
+}
+
+function escapeRegExp(string) {
+    return string.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+}
+
+function replaceAll(find, replace, str) {
+    return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
+
+function nl2eol(data){
+    return replaceAll('\\n', endOfLine, data);
+}
+
+var http = require('http');
+var qs = require('querystring');
+var mailer = require('nodemailer');
+var endOfLine = require('os').EOL;
+
+var smtpTransport = mailer.createTransport("SMTP", smtpOptions);
+
+http.createServer(function(req, res){
+    if(req.method == requestType){
+        // do stuff
+        var body = '';
+
+        req.on('data', function(data){
+            body += data;
+            if(body > 1e6){
+                // request too large
+                res.writeHead(413, {"Content-Type": "text/plain"});
+                res.write('Data submission was too large.');
+                res.end();
+                res.connection.destroy();
+            }
+        });
+
+        req.on('end', function(){
+            var DATA = qs.parse(body);
+
+            if(DATA != null){
+                if(in_array(DATA.to, array_keys(allowed_emails))){
+                    // override mailOptions with DATA values
+                    mailerOptions.to = allowed_emails[DATA.to].join(',');
+                    if(DATA.subj != null){
+                        mailerOptions.subject = DATA.subj;
+                    }
+                    mailerOptions.text = nl2eol(DATA.body);
+                    // good to go - send message
+                    smtpTransport.sendMail(mailerOptions, function(error, result){
+                        if(!error){
+                            res.writeHead(200, {"Content-Type": "text/plain"});
+                            res.write('Transaction complete. [' + result.message + ']');
+                        } else {
+                            // email failure
+                            console.log(error);
+                            res.writeHead(503, {"Content-Type": "text/plain"});
+                            res.write('Unable to send message. [' + error.message + ']');
+                            res.end();
+                        }
+                        smtpTransport.close();
+                    });
+                } else {
+                    // invalid email address
+                    res.writeHead(400, {"Content-Type": "text/plain"});
+                    res.write('Invalid addressee [' + DATA.to + '].');
+                }
+            } else {
+                // no data
+                res.writeHead(400, {"Content-Type": "text/plain"});
+                res.write('No data supplied.');
+                res.end();
+            }
+            // always end
+            res.end();
+        });
+
+    } else {
+        // unsupported method
+        res.writeHead(501, {"Content-Type": "text/plain"});
+        res.write('Unsupported method.  Use ' + requestType + '.');
+        res.end();
+    }
+}).listen(httpPort);
